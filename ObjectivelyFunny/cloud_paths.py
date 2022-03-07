ab166434eb063cc811dadfe8b34855bbcfb157b2
Reinis Melbardis <rmelbardis@gmail.com>
2022-03-07 11:34:16 +0000

    debug data function

--- ObjectivelyFunny/cloud_paths.py
+++ ObjectivelyFunny/cloud_paths.py
@@ -1,7 +1,8 @@
 BUCKET_NAME = 'wagon-data-805-melbardis'
-BUCKET_DATA_PATH = 'obj-fun-data/'
+BUCKET_DATA_PATH = 'obj-fun-data'
+DATA_NAME = 'fully_stripped_df_bo.json'
 STORAGE_LOCATION = 'models/obj-fun/model.joblib'
 
-PATH_TO_DATA = f"gs://{BUCKET_NAME}/{BUCKET_DATA_PATH}"
+PATH_TO_DATA = f"gs://{BUCKET_NAME}/{BUCKET_DATA_PATH}/{DATA_NAME}"
 PATH_TO_LOCAL_MODEL = 'model.joblib'
 PATH_TO_GCP_MODEL = f"gs://{BUCKET_NAME}/{STORAGE_LOCATION}"
--- ObjectivelyFunny/data.py
+++ ObjectivelyFunny/data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+
 from ObjectivelyFunny import cloud_paths
 
 def get_data():
@@ -6,20 +7,7 @@ def get_data():
     df = pd.read_json(cloud_paths.PATH_TO_DATA)
     return df
 
-def clean_data(df, test=False):
-    df = df.dropna(how='any', axis='rows')
-    df = df[(df.dropoff_latitude != 0) | (df.dropoff_longitude != 0)]
-    df = df[(df.pickup_latitude != 0) | (df.pickup_longitude != 0)]
-    if "fare_amount" in list(df):
-        df = df[df.fare_amount.between(0, 60)]
-    df = df[df.passenger_count < 8]
-    df = df[df.passenger_count >= 0]
-    df = df[df["pickup_latitude"].between(left=40, right=42)]
-    df = df[df["pickup_longitude"].between(left=-74.3, right=-72.9)]
-    df = df[df["dropoff_latitude"].between(left=40, right=42)]
-    df = df[df["dropoff_longitude"].between(left=-74, right=-72.9)]
-    return df
-
 
 # if __name__ == '__main__':
-#     df = clean_data(get_data(nrows=10_000))
+#     df = get_data()
+#     print(df.head())
