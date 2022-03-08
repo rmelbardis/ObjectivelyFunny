24cd2150c2c0d95b4834564d2924588a35e17f2f
Yuqing Wang <bibolikepeaches@gmail.com>
2022-03-08 15:25:30 +0000

    age cloud page v1 done

--- app/age_cloud.py
+++ app/age_cloud.py
@@ -5,68 +5,70 @@ from PIL import Image
 # ☁ Compare decade word clouds
 ------------------------------
 '''
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-CSS = """
-h1 {
-    color: white;
-}
-p {
-    color: white;
-    font-size: 30px;
-    font-family: monospace;
-}
-.stApp {
-    background-image: url(https://i.ibb.co/H4FjXXj/27598206.jpg);
-    background-size: cover;
-}
-"""
-st.write(f'<style>{CSS}</style>', unsafe_allow_html=True)
+add_selectbox = st.sidebar.selectbox(
+    "Jump to",
+    ("Main", "About", "Comedy Bot", "Clouds")
+)
 
+age_group_dict = {'Under 30': 'U30',
+                  '30-39': '30',
+                  '40-49': '40',
+                  '50-59': '50',
+                  '60-69': '60',
+                  'Over 60': 'U60'}
 
-st.write('⌛ Which decades do you want to compare')
-
-col1, col2 = st.columns(2)
-
-with col1:
-    option1 = st.selectbox(
-     '',
-     ('Under 30', '30-40', '41-50', '51-60', 'Over 60'))
-    st.write('Age group １:', option1)
-
-with col2:
-    option2 = st.slider('Select the second age group', min_value=1980,  max_value =2020,
-                    step=10, key=2)
-    st.write('Age group ２:', option2)
-
-image1 = Image.open(f'../cloud-images/{option1}_cloud.png')
-
-if option1 == option2:
-    st.warning('Change one of your options to compare different decades')
-    if st.button('Continue with one decade'):
-        st.write('Results generated 🎉')
-        st.image(image1, width=500, caption=f'{option1}s word cloud')
-    else:
-        st.write('😞 Cloud not generated yet.')
-
-else:
-    image2 = Image.open(f'../cloud-images/{option2}_cloud.png')
-    # image = Image.open('../ObjectivelyFunny/images/emoji.png')
-
-
+with st.container():
     col1, col2 = st.columns(2)
 
+    with col1:
+        option1 = st.selectbox(
+        '',
+        ('Under 30', '30-39', '40-49', '50-59', '60-69', 'Over 60'),
+        key=1)
+        st.write('Age group １:', option1)
+        result1= age_group_dict[option1]
 
-    """
-        ---------------------------------
-    """
-    if st.button('Generate'):
-        st.write('Results generated 🎉')
-        'Here is your cloud:'
-        with col1:
-            st.header("pic1")
-            st.image(image1, width=300, caption=f'{option1}s word cloud')
-        with col2:
-            st.header("pic2")
-            st.image(image2, width=300, caption=f'{option2}s word cloud')
+    with col2:
+        option2 = st.selectbox(
+        '',
+        ('Under 30', '30-39', '40-49', '50-59', '60-69', 'Over 60'),
+        key=2)
+        st.write('Age group ２:', option2)
+        result2= age_group_dict[option2]
+
+
+    image1 = Image.open(f'../cloud-images/{result1}_cloud.png')
+
+    if option1 == option2:
+        st.warning('Change one of your options to compare different age groups')
+        if st.button('Continue with one age group'):
+            st.write('Results generated 🎉')
+            st.header(f'People aged {option1}')
+            st.image(image1, width=700)
+        else:
+            st.write('😞 Cloud not generated yet.')
+
     else:
-        st.write('😞 Cloud not generated yet.')
+        image2 = Image.open(f'../cloud-images/{result2}_cloud.png')
+
+        col1, col2 = st.columns(2)
+
+        """
+            ---------------------------------
+        """
+        if st.button('Generate'):
+            st.write('Results generated 🎉')
+            """
+            ---------------------------------
+            """
+            with col1:
+                st.header(f'People aged {option1}')
+                st.image(image1, width=550)
+            with col2:
+                st.header(f'People aged {option2}')
+                st.image(image2, width=550)
+        else:
+            st.write('😞 Cloud not generated yet.')
