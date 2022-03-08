f9b2124fb752506fe07d4c8cf6165d2b4d4e8324
Yuqing Wang <bibolikepeaches@gmail.com>
2022-03-08 13:06:56 +0000

    starting with streamlit pages

---
+++ app/decade_cloud.py
@@ -0,0 +1,71 @@
+import streamlit as st
+from PIL import Image
+
+'''
+# ☁ Compare decade word clouds
+------------------------------
+'''
+
+CSS = """
+h1 {
+    color: white;
+}
+p {
+    color: white;
+    font-size: 30px;
+    font-family: monospace;
+}
+.stApp {
+    background-image: url(https://i.ibb.co/H4FjXXj/27598206.jpg);
+    background-size: cover;
+}
+"""
+st.write(f'<style>{CSS}</style>', unsafe_allow_html=True)
+
+
+st.write('⌛ Which decades do you want to compare')
+
+col1, col2 = st.columns(2)
+
+with col1:
+    option1 = st.slider('Select the first decade', min_value=1980,  max_value =2020,
+                    step=10, key=1)
+    st.write('Decade １:', f'{option1}s')
+
+with col2:
+    option2 = st.slider('Select the second decade', min_value=1980,  max_value =2020,
+                    step=10, key=2)
+    st.write('Decade ２:', f'{option2}s')
+
+image1 = Image.open(f'../cloud-images/{option1}_cloud.png')
+
+if option1 == option2:
+    st.warning('Change one of your options to compare different decades')
+    if st.button('Continue with one decade'):
+        st.write('Results generated 🎉')
+        st.image(image1, width=500, caption=f'{option1}s word cloud')
+    else:
+        st.write('😞 Cloud not generated yet.')
+
+else:
+    image2 = Image.open(f'../cloud-images/{option2}_cloud.png')
+    # image = Image.open('../ObjectivelyFunny/images/emoji.png')
+
+
+    col1, col2 = st.columns(2)
+
+
+    """
+        ---------------------------------
+    """
+    if st.button('Generate'):
+        st.write('Results generated 🎉')
+        'Here is your cloud:'
+        with col1:
+            st.header("pic1")
+            st.image(image1, width=300, caption=f'{option1}s word cloud')
+        with col2:
+            st.header("pic2")
+            st.image(image2, width=300, caption=f'{option2}s word cloud')
+    else:
+        st.write('😞 Cloud not generated yet.')
--- app/test_cloud.py
+++ app/test_cloud.py
@@ -1,22 +1,6 @@
 import streamlit as st
 from PIL import Image
 
-
-# new_title = '<p style="font-family:sans-serif; color:White; font-size: 80px;">Examples</p>'
-# st.markdown(new_title, unsafe_allow_html=True)
-
-option = st.selectbox(
-    'Gender',
-    ('Female', 'Male', 'Gender queer', 'All'))
-
-st.write('🤔 You selected:', option)
-
-option = st.selectbox(
-    'Age',
-    ('Under 25', '26-35', '36-45', 'Over 45'))
-
-st.write('🤔 You selected:', option)
-
 '''
 # ☁ Some examples of word clouds
 ------------------------------
@@ -38,34 +22,14 @@ p {
 """
 st.write(f'<style>{CSS}</style>', unsafe_allow_html=True)
 
-
-st.write('⌛ Which decade would you like to see?')
-
 option = st.selectbox(
-    '',
-    ('1960s', '1970s', '1980s', '1990s','2000s','2010s','2020s'))
+    'Gender',
+    ('Female', 'Male', 'Gender queer', 'All'))
 
 st.write('🤔 You selected:', option)
 
-option = option.replace('s', '')
-image = Image.open(f'../cloud-images/{option}_cloud.png')
-# image = Image.open('../ObjectivelyFunny/images/emoji.png')
+option = st.selectbox(
+    'Age',
+    ('Under 25', '26-35', '36-45', 'Over 45'))
 
-
-col1, col2 = st.columns(2)
-
-
-"""
-    ---------------------------------
-"""
-if st.button('Generate'):
-    st.write('Results generated 🎉')
-    'Here is your cloud:'
-    with col1:
-        st.header("pic1")
-        st.write('another pic?')
-    with col2:
-        st.header("pic2")
-        st.image(image, width=500, caption=f'{option}s word cloud')
-else:
-    st.write('😞 Cloud not generated yet.')
+st.write('🤔 You selected:', option)
