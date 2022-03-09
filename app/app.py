0ef2c849370fcc3c0875cfe975c0cc7abaacb099
Yuqing Wang <bibolikepeaches@gmail.com>
2022-03-09 11:36:07 +0000

    webpages updated

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
 from multipage import MultiPage
-import age_cloud, decade_cloud, gender_cloud, about, cloud_land, bot
+import about, cloud_land, bot
 
 # Import the required module for text
 # to speech conversion
@@ -15,12 +15,8 @@ st.title("Objectively Funny")
 
 # Add all your applications (pages) here
 app.add_page("About",about.app)
-# app.add_page("Age", age_cloud.app)
-# app.add_page("Decade", decade_cloud.app)
-# app.add_page("Gender", gender_cloud.app)
 app.add_page("Clouds",cloud_land.app)
 app.add_page("Bot",bot.app)
-
 
 # The main app
 app.run()
