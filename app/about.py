728158c8e780a7fe47c4b98137fb4d8010c4196c
Yuqing Wang <bibolikepeaches@gmail.com>
2022-03-10 10:17:48 +0000

    updated about page

--- app/about.py
+++ app/about.py
@@ -13,21 +13,27 @@ def app():
     '''
 
     st.header('**Welcome to our home page!**')
-    st.write('This project is made by 4 students from Le Wagon Data Science Bootcamp batch 805.')
+    st.markdown("<h1 style='font-size: 20px; color: white;'>We recommend the use of dark mode. You can change the theme in the settings (on your right).</h1>",
+                    unsafe_allow_html=True)
 
     st.subheader('**Our project**')
-    st.write('...has sourced and processed a wide range of stand-up scripts to analyse comedy and comedians.')
+    st.write('This project was made by 4 students from the Le Wagon Data Science Bootcamp batch 805.')
+    st.write('We have sourced and processed a wide range of stand-up scripts to analyse comedy and comedians.')
+    st.write("Please check our [GitHub page](%s)" % gh_url)
 
     st.subheader('Here you can find:')
-    st.write("1) Word Clouds on comedy topics")
-    st.write("2) An AI bot that tells you a joke based on your text input")
+    st.write("1) Word clouds on comedy topics")
+    st.write("2) Robecca: an AI bot that tells you jokes based on your text input")
 
     st.subheader('Need help?')
     st.write('- Use the **sidebar** on the left to go to different sections.')
     st.write('- 📪 Please speak to Rei if you have any questions.')
 
     st.header('About our data')
-    st.write("- Please check our GitHub page here: [link](%s)" % gh_url)
+    st.subheader('- Data Overview')
+    st.write('- Data Size: 555 individual transcripts')
+    st.write('- Average age of comedians: 41')
+    st.write("- 84% Male, 15% Female, 1% Gender queer")
 
     st.subheader('Data preprocessing')
     st.image(image1, width=600)
@@ -37,17 +43,20 @@ def app():
     st.subheader('Data sources:')
 
     url1 = "http://scrapsfromtheloft.com/stand-up-comedy-scripts/"
-    st.write("- Scraps From the Loft [link](%s)" % url1)
+    st.write("- [Scraps From the Loft](%s)" % url1)
     url2 = 'https://subsaga.com/bbc/browse/genre/comedy/standup/?page=0'
-    st.write("- Subsaga [link](%s)" % url2)
+    st.write("- [Subsaga](%s)" % url2)
     url3 = 'https://www.themoviedb.org/?language=en-GB'
-    st.write("- The Movie Database (TMDB) [link](%s)" % url3)
+    st.write("- [The Movie Database (TMDB)](%s)" % url3)
 
     st.subheader('Packages and APIs:')
     url4 = 'https://pypi.org/project/tmdbsimple/'
-    st.write("- tmdbsimple [link](%s)" % url4)
+    st.write("- [tmdbsimple](%s)" % url4)
     url5 = 'https://openai.com/api/'
-    st.write('- GPT-2 [link](%s)' % url5)
+    st.write('- [GPT-2](%s)' % url5)
+    url6 = 'https://github.com/minimaxir/gpt-2-simple'
+    st.write('- [gpt-2-simple](%s)' % url6)
 
     st.subheader('Special thanks:')
     st.write("- Amanda, Julio, Marie, Mohamad, Vinny, Yannis, Yassine")
+    st.write("- and all lecturers and TAs at Le Wagon London")
